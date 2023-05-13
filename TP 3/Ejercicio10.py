3a14e16a71ace1e8edc853b3c3ccada8591c1743
unknown <francisco.daniel.gomez.97@gmail.com>
2023-05-13 17:12:23 -0300

    Actualización para entrega

--- TP 3/Ejercicio10.py
+++ TP 3/Ejercicio10.py
@@ -8,3 +8,36 @@ la palabra ‘Python’, si perder datos en la cola;
 c. utilizar una pila para almacenar temporáneamente las notificaciones producidas entre las
 11:43 y las 15:57, y determinar cuántas son.
 '''
+
+from pila import Pila
+from cola import Cola
+
+Notificaciones = [
+    {'Hora': 11.43, 'Aplicación': 'Facebook','Mensaje':'a'},
+    {'Hora': 12.43, 'Aplicación': 'Twitter','Mensaje':'a' },
+    {'Hora': 13.43, 'Aplicación': 'Twitter','Mensaje': 'Phyton'},
+    {'Hora': 14.43, 'Aplicación': 'Instagram','Mensaje': 'a'},
+    {'Hora': 15.57, 'Aplicación': 'LinkedIn','Mensaje': 'a'},
+    {'Hora': 16.00, 'Aplicación': 'Facebook','Mensaje':'a' }
+    ]
+
+pila = Pila()
+cola = Cola()
+cola_auxiliar = Cola()
+cola_twitter = Cola()
+
+for x in Notificaciones:
+    cola.arrive(x)
+
+while cola.size() > 0:
+    Notificación = cola.atention()
+    if (Notificación['Hora'] >= 11.43) and (Notificación['Hora'] <= 15.57):
+        pila.push(Notificación)
+    if Notificación['Aplicación'] != 'Facebook':
+        cola_auxiliar.arrive(Notificación)
+    if (Notificación['Aplicación'] == 'Twitter') and  ('Python' in Notificación['Mensaje']):
+        cola_twitter.arrive(Notificación)
+        #print ('Hora: ', Notificación['Hora'], 'Aplicación: ', Notificación['Aplicación'], 'Mensaje: ', Notificación['Mensaje'])
+        print (Notificación)
+
+print ('La cantidad de notificaciones que se almacenaron fue de: ', (pila.size()))
--- TP 3/Ejercicio22.py
+++ TP 3/Ejercicio22.py
@@ -1,7 +1,7 @@
 '''
 Se tienen una cola con personajes de Marvel Cinematic Universe (MCU), de los cuales se conoce
 el nombre del personaje, el nombre del superhéroe y su género (Masculino M y Femenino
-F) –por ejemplo {Tony Stark, Iron Man, M}, {Steve Rogers, Capitán América, M}, {Natasha Romanoff,
+F) por ejemplo {Tony Stark, Iron Man, M}, {Steve Rogers, Capitán América, M}, {Natasha Romanoff,
 Black Widow, F}, etc., desarrollar un algoritmo que resuelva las siguientes actividades:
 a. determinar el nombre del personaje de la superhéroe Capitana Marvel;
 b. mostrar los nombre de los superhéroes femeninos;
@@ -12,3 +12,37 @@ con la letra S;
 f. determinar si el personaje Carol Danvers se encuentra en la cola e indicar su nombre
 de superhéroes.
 '''
+from cola import Cola
+
+Personajes_MCU = [
+    {'Nombre_Personaje': 'Tony Stark','Nombre_Superheroe': 'Iron Man','Género': 'M'},
+    {'Nombre_Personaje': 'Steve Rogers','Nombre_Superheroe': 'Capitán América','Género': 'M'},
+    {'Nombre_Personaje': 'Natasha Romanoff','Nombre_Superheroe': 'Black Widow','Género': 'F'},
+    {'Nombre_Personaje': 'Brie Larson','Nombre_Superheroe': 'Capitana América','Género': 'F'},
+    {'Nombre_Personaje': 'Scott Lang','Nombre_Superheroe': 'Ant-Man','Género': 'M'},
+    {'Nombre_Personaje': 'Carol Danvers','Nombre_Superheroe': 'x','Género': 'F'}]
+
+cola = Cola()
+
+for x in Personajes_MCU:
+    cola.arrive(x)
+
+while cola.size() > 0:
+    Personaje = cola.atention()
+    
+    if Personaje['Nombre_Superheroe'] == 'Capitana América':
+        print ('El nombre de la Capitana América es: ', Personaje['Nombre_Personaje'])
+    
+    if Personaje['Género'] == 'F':
+        print ('El nombre del superheroe es: ', Personaje['Nombre_Superheroe'])
+    elif Personaje['Género'] == 'M':
+        print ('El nombre del personaje es: ', Personaje['Nombre_Personaje'])
+
+    if Personaje['Nombre_Personaje'] == 'Scott Lang':
+        print ('El nombre del superheroe es: ', Personaje['Nombre_Superheroe'])
+    
+    if (Personaje['Nombre_Personaje'][0] == 'S') or (Personaje['Nombre_Superheroe'][0] == 'S'):
+        print ('Nombre del personaje: ', Personaje['Nombre_Personaje'], ', ', 'Nombre de superheroe: ', Personaje['Nombre_Superheroe'],', ', 'Género: ', Personaje['Género'])
+
+    if Personaje['Nombre_Personaje'] == 'Carol Danvers':
+        print ('Carol Danvers se encuentra en la cola, y su nombre de superheroe es: ', Personaje['Nombre_Superheroe'])
