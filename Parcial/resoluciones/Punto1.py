7a55709456fdc7b65966fdd188e5b544071c9735
unknown <francisco.daniel.gomez.97@gmail.com>
2023-06-13 21:08:46 -0300

    Cambio del nombre del recursivo

--- Parcial/resoluciones/Punto1.py
+++ Parcial/resoluciones/Punto1.py
@@ -2,13 +2,13 @@ V =['wea','wea','wea','wea','wea','a','a','a','a','a','a','a']
 
 palabra=input('Ingrese palabra a buscar: ')
 
-def contador_palabra(vector,palabra):
+def cont_palabras(vector,palabra):
     if len(vector)==0:
         return 0
     elif vector[-1]==palabra:
-        return 1 + contador_palabra(vector[:-1],palabra)
+        return 1 + cont_palabras(vector[:-1],palabra)
     else:
-        return contador_palabra(vector[:-1],palabra)
+        return cont_palabras(vector[:-1],palabra)
 
-Contador= contador_palabra(V,palabra)
+Contador= cont_palabras(V,palabra)
 print ("La palabra seleccionada se ejecutó: ", Contador)
